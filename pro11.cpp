058eac64d2345961a95458f059a6376d7cb7d45b
Rachit2000 <rachitchauhan2302@gmail.com>
2018-04-03 14:56:57 +0530

    commiting on master branch first time

---
+++ pro11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int first, second, third;
+	cout<< "enter two numbers"<<endl;
+	cin>> first>> second;
+	third= first+second;
+	cout<< first << "+" << "=" third;
+	return 0;
+	}
